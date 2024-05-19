d5f25715393a4f47bd20f7ee9d84f281cb879fab
SX-Code <songwuxingxox@outlook.com>
2024-05-19 12:16:21 +0800

    添加快捷启动应用配置

--- app/src/main/java/com/swx/adbcontrol/utils/Constant.java
+++ app/src/main/java/com/swx/adbcontrol/utils/Constant.java
@@ -12,7 +12,10 @@ public class Constant {
     public static final String KEY_QUICK_ACCESS_APP_ONLINE_URL = "key_quick_access_app_online_url";
     public static final String DEFAULT_QUICK_ACCESS_APP_ONLINE_URL = "https://gitee.com/SX-Code/adbremote-atv/raw/master/apps.json";
     public static final String VALUE_ACTIVITY_APP_PRIORITY = "value_activity_app_priority";
-    public static final String URL_LATEST_APK_INFO = "https://gitee.com/SX-Code/adbremote-atv/raw/master/latest_apk_info.txt";
+    public static final String URL_PREFIX_CN = "https://gitee.com/SX-Code/adbremote-atv/raw/master/";
+    public static final String URL_PREFIX_EN = "https://gitee.com/SX-Code/adbremote-atv/raw/master/";
+    public static final String URL_LATEST_APK_INFO_EN = URL_PREFIX_EN + "/latest_apk_info.txt";
+    public static final String URL_LATEST_APK_INFO_CN = URL_PREFIX_CN + "latest_apk_info.txt";
     public static final String URL_CHECK_VERSION = "";
     public static final String SAVE_FILE_NAME = "ADB_Remote_ATV_%s.apk";
     public static final String URL_GITHUB_CODE_PAGE = "https://github.com/SX-Code/ADBRemoteATV";
